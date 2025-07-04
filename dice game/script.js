496ed5630fd46fccb980e91e2037d3e919084b04
urprajesh23 <151101211+urprajesh23@users.noreply.github.com>
2025-07-04 09:46:09 +0530

    script.js

--- dice game/script.js
+++ dice game/script.js
@@ -1,67 +1,86 @@
-let scores, currentScore, activePlayer, playing;
+var selectPlayer = 1;
+const rollButton = document.getElementById("rollButton");
+const image = document.getElementById("dice");
+const currentScorePlayer1 = document.getElementById("currentplayer-1");
+const currentScorePlayer2 = document.getElementById("currentplayer-2");
+const scorePlayer1 = document.getElementById("scoreplayer-1");
+const scorePlayer2 = document.getElementById("scoreplayer-2");
+const game = document.getElementById("game");
+const holdButton = document.getElementById("holdButton");
+const reset = document.getElementById("reset");
 
-const diceEl = document.getElementById('dice');
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-
-function init() {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  diceEl.style.display = 'none';
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-}
-
-init();
-
-function switchPlayer() {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-}
-
-document.getElementById('btn--roll').addEventListener('click', () => {
-  if (playing) {
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.src = `dice-${dice}.jpg`;
-    diceEl.style.display = 'block';
-
-    if (dice !== 1) {
-      currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+rollButton.addEventListener("click", () => {
+  randomNumber = Math.floor(Math.random() * 6) + 1;
+  image.src = `/roll a dice/dice/dice${randomNumber}.jpg `;
+  if (scorePlayer1 >= 100) {
+    alert("player1 wins");
+  }
+  if (scorePlayer2 >= 100) {
+    alert("player2 wins");
+  }
+  if (randomNumber === 1) {
+    if (selectPlayer == 1) {
+      scorePlayer1.innerHTML =
+        Number(scorePlayer1.innerHTML) + Number(currentScorePlayer1.innerHTML);
+      currentScorePlayer1.innerHTML = 0;
+      game.style.background = `linear-gradient(
+      90deg,
+      rgba(186, 151, 176, 1) 50%,
+      rgba(242, 218, 234, 1) 50%
+    )`;
     } else {
-      switchPlayer();
+      scorePlayer2.innerHTML =
+        Number(scorePlayer2.innerHTML) + Number(currentScorePlayer2.innerHTML);
+      currentScorePlayer2.innerHTML = 0;
+      game.style.background = `linear-gradient(
+      90deg,
+      rgba(242, 218, 234, 1) 50%,
+      rgba(186, 151, 176, 1) 50%
+    )`;
+    }
+
+    selectPlayer = selectPlayer === 1 ? 0 : 1;
+  } else {
+    if (selectPlayer == 1) {
+      currentScorePlayer1.innerHTML = randomNumber;
+    } else {
+      currentScorePlayer2.innerHTML = randomNumber;
     }
   }
 });
 
-document.getElementById('btn--hold').addEventListener('click', () => {
-  if (playing) {
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-
-    if (scores[activePlayer] >= 100) {
-      playing = false;
-      diceEl.style.display = 'none';
-      alert(`🎉 Player ${activePlayer + 1} wins!`);
-    } else {
-      switchPlayer();
-    }
+holdButton.addEventListener("click", () => {
+  if (selectPlayer == 1) {
+    scorePlayer1.innerHTML =
+      Number(scorePlayer1.innerHTML) + Number(currentScorePlayer1.innerHTML);
+    currentScorePlayer1.innerHTML = 0;
+    game.style.background = `linear-gradient(
+      90deg,
+      rgba(186, 151, 176, 1) 50%,
+      rgba(242, 218, 234, 1) 50%
+    )`;
+  } else {
+    scorePlayer2.innerHTML =
+      Number(scorePlayer2.innerHTML) + Number(currentScorePlayer2.innerHTML);
+    currentScorePlayer2.innerHTML = 0;
+    game.style.background = `linear-gradient(
+      90deg,
+      rgba(242, 218, 234, 1) 50%,
+      rgba(186, 151, 176, 1) 50%
+    )`;
   }
+  selectPlayer = selectPlayer === 1 ? 0 : 1;
 });
 
-document.getElementById('btn--new').addEventListener('click', init);
+reset.addEventListener("click", () => {
+  currentScorePlayer1.innerHTML = 0;
+  currentScorePlayer2.innerHTML = 0;
+  scorePlayer1.innerHTML = 0;
+  scorePlayer2.innerHTML = 0;
+  selectPlayer = 1;
+  game.style.background = `linear-gradient(
+      90deg,
+      rgba(242, 218, 234, 1) 50%,
+      rgba(186, 151, 176, 1) 50%
+    )`;
+});
